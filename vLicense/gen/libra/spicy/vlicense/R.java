2e95657f72d4086af90499902fa1786c4ae2a22c
libros <libra.spicy@gmail.com>
2012-11-07 19:42:59 +1100

    第一个界面的雏形

--- vLicense/gen/libra/spicy/vlicense/R.java
+++ vLicense/gen/libra/spicy/vlicense/R.java
@@ -10,31 +10,119 @@ package libra.spicy.vlicense;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int bg=0x7f040008;
+        public static final int bg_gray=0x7f040005;
+        public static final int black=0x7f040003;
+        public static final int black2=0x7f040018;
+        public static final int blue=0x7f040015;
+        public static final int buttom_buttom=0x7f04001c;
+        public static final int buttom_centre=0x7f04001b;
+        public static final int buttom_top=0x7f04001a;
+        public static final int click_color=0x7f040019;
+        public static final int coffee=0x7f040017;
+        public static final int gray=0x7f04000f;
+        public static final int light_blue=0x7f040010;
+        public static final int light_gray=0x7f040011;
+        public static final int light_orange=0x7f040016;
+        public static final int lightblue=0x7f040009;
+        public static final int lightorange=0x7f04000a;
+        public static final int nomalGray=0x7f040014;
+        public static final int orange=0x7f040006;
+        public static final int orange_red=0x7f040012;
+        public static final int red=0x7f04000b;
+        public static final int transparent=0x7f040007;
+        public static final int transparentdarkgray=0x7f04000c;
+        public static final int transparentgray=0x7f04000d;
+        public static final int txt_blue=0x7f040000;
+        public static final int txt_gray=0x7f040001;
+        public static final int txt_orange=0x7f040004;
+        public static final int white=0x7f040002;
+        public static final int whiteGray=0x7f040013;
+        public static final int yellow=0x7f04000e;
+    }
     public static final class dimen {
-        public static final int padding_large=0x7f040002;
-        public static final int padding_medium=0x7f040001;
-        public static final int padding_small=0x7f040000;
+        public static final int padding_large=0x7f050002;
+        public static final int padding_medium=0x7f050001;
+        public static final int padding_small=0x7f050000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int arrow1=0x7f020000;
+        public static final int back=0x7f020001;
+        public static final int back_down=0x7f020002;
+        public static final int bg_btn_back=0x7f020003;
+        public static final int bg_checkbox_icon_menu_0=0x7f020004;
+        public static final int bg_checkbox_icon_menu_1=0x7f020005;
+        public static final int bg_checkbox_icon_menu_2=0x7f020006;
+        public static final int bg_checkbox_icon_menu_3=0x7f020007;
+        public static final int bg_checkbox_menus=0x7f020008;
+        public static final int bg_checkbox_menus_up=0x7f020009;
+        public static final int big_button_up=0x7f02000a;
+        public static final int bottom1=0x7f02000b;
+        public static final int home_btn_bg=0x7f02000c;
+        public static final int ic_action_search=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int ic_pulltorefresh_arrow=0x7f02000f;
+        public static final int info_icon_3=0x7f020010;
+        public static final int label4=0x7f020011;
+        public static final int label_1=0x7f020012;
+        public static final int label_2=0x7f020013;
+        public static final int label_3=0x7f020014;
+        public static final int label_four_one=0x7f020015;
+        public static final int label_one_one=0x7f020016;
+        public static final int label_three_one=0x7f020017;
+        public static final int label_two_one=0x7f020018;
+        public static final int menu_icon_0_normal=0x7f020019;
+        public static final int menu_icon_0_pressed=0x7f02001a;
+        public static final int menu_icon_1_normal=0x7f02001b;
+        public static final int menu_icon_1_pressed=0x7f02001c;
+        public static final int menu_icon_2_normal=0x7f02001d;
+        public static final int menu_icon_2_pressed=0x7f02001e;
+        public static final int menu_icon_3_normal=0x7f02001f;
+        public static final int menu_icon_3_pressed=0x7f020020;
+        public static final int mm_trans=0x7f020021;
+        public static final int slidebar=0x7f020022;
+        public static final int tips=0x7f020023;
+        public static final int top_1=0x7f020024;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int btn_leftTop=0x7f090002;
+        public static final int btn_rightTop=0x7f090004;
+        public static final int item_image=0x7f090000;
+        public static final int item_textview=0x7f090001;
+        public static final int main_grid_view=0x7f090005;
+        public static final int main_tab_addExam=0x7f09000b;
+        public static final int main_tab_group=0x7f09000a;
+        public static final int main_tab_message=0x7f09000d;
+        public static final int main_tab_myExam=0x7f09000c;
+        public static final int main_tab_settings=0x7f09000e;
+        public static final int menu_settings=0x7f09000f;
+        public static final int top_tab_button_left=0x7f090007;
+        public static final int top_tab_button_middle=0x7f090008;
+        public static final int top_tab_button_right=0x7f090009;
+        public static final int top_tab_group=0x7f090006;
+        public static final int tv_head=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int grid_cell=0x7f030001;
+        public static final int head=0x7f030002;
+        public static final int main_grid_view=0x7f030003;
+        public static final int main_header=0x7f030004;
+        public static final int main_tab=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int activity_main=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int title_activity_main=0x7f050003;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
+        public static final int title_activity_main=0x7f060003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
+        public static final int MMTabButton=0x7f070001;
+        public static final int MMTabButton1=0x7f070002;
     }
 }
--- vLicense/src/libra/spicy/vlicense/MainActivity.java
+++ vLicense/src/libra/spicy/vlicense/MainActivity.java
@@ -1,24 +1,42 @@
 package libra.spicy.vlicense;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		GridView gridView = (GridView) findViewById(R.id.main_grid_view);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		ArrayList<HashMap<String, Object>> gridviewData = new ArrayList<HashMap<String, Object>>();
 
-    
+		for (int i = 1; i < 10; i++) {
+			HashMap<String, Object> grid = new HashMap<String, Object>();
+			grid.put("ItemImage", R.drawable.ic_launcher);
+			grid.put("ItemText", "Good" + i);
+			gridviewData.add(grid);
+		}
+
+		SimpleAdapter gridViewAdapter = new SimpleAdapter(this, gridviewData,
+				R.layout.grid_cell, new String[] { "ItemImage", "ItemText" },
+				new int[] { R.id.item_image, R.id.item_textview });
+
+		gridView.setAdapter(gridViewAdapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
 }
