d5743a0e5aec374234a96b7c3b3baa68e35214e7
libros <libra.spicy@gmail.com>
2012-11-08 21:07:06 +1100

    第一层界面粗略完成

--- vLicense/gen/libra/spicy/vlicense/R.java
+++ vLicense/gen/libra/spicy/vlicense/R.java
@@ -8,43 +8,47 @@
 package libra.spicy.vlicense;
 
 public final class R {
+    public static final class anim {
+        public static final int in_from_right=0x7f040000;
+        public static final int out_to_left=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int bg=0x7f040008;
-        public static final int bg_gray=0x7f040005;
-        public static final int black=0x7f040003;
-        public static final int black2=0x7f040018;
-        public static final int blue=0x7f040015;
-        public static final int buttom_buttom=0x7f04001c;
-        public static final int buttom_centre=0x7f04001b;
-        public static final int buttom_top=0x7f04001a;
-        public static final int click_color=0x7f040019;
-        public static final int coffee=0x7f040017;
-        public static final int gray=0x7f04000f;
-        public static final int light_blue=0x7f040010;
-        public static final int light_gray=0x7f040011;
-        public static final int light_orange=0x7f040016;
-        public static final int lightblue=0x7f040009;
-        public static final int lightorange=0x7f04000a;
-        public static final int nomalGray=0x7f040014;
-        public static final int orange=0x7f040006;
-        public static final int orange_red=0x7f040012;
-        public static final int red=0x7f04000b;
-        public static final int transparent=0x7f040007;
-        public static final int transparentdarkgray=0x7f04000c;
-        public static final int transparentgray=0x7f04000d;
-        public static final int txt_blue=0x7f040000;
-        public static final int txt_gray=0x7f040001;
-        public static final int txt_orange=0x7f040004;
-        public static final int white=0x7f040002;
-        public static final int whiteGray=0x7f040013;
-        public static final int yellow=0x7f04000e;
+        public static final int bg=0x7f050008;
+        public static final int bg_gray=0x7f050005;
+        public static final int black=0x7f050003;
+        public static final int black2=0x7f050018;
+        public static final int blue=0x7f050015;
+        public static final int buttom_buttom=0x7f05001c;
+        public static final int buttom_centre=0x7f05001b;
+        public static final int buttom_top=0x7f05001a;
+        public static final int click_color=0x7f050019;
+        public static final int coffee=0x7f050017;
+        public static final int gray=0x7f05000f;
+        public static final int light_blue=0x7f050010;
+        public static final int light_gray=0x7f050011;
+        public static final int light_orange=0x7f050016;
+        public static final int lightblue=0x7f050009;
+        public static final int lightorange=0x7f05000a;
+        public static final int nomalGray=0x7f050014;
+        public static final int orange=0x7f050006;
+        public static final int orange_red=0x7f050012;
+        public static final int red=0x7f05000b;
+        public static final int transparent=0x7f050007;
+        public static final int transparentdarkgray=0x7f05000c;
+        public static final int transparentgray=0x7f05000d;
+        public static final int txt_blue=0x7f050000;
+        public static final int txt_gray=0x7f050001;
+        public static final int txt_orange=0x7f050004;
+        public static final int white=0x7f050002;
+        public static final int whiteGray=0x7f050013;
+        public static final int yellow=0x7f05000e;
     }
     public static final class dimen {
-        public static final int padding_large=0x7f050002;
-        public static final int padding_medium=0x7f050001;
-        public static final int padding_small=0x7f050000;
+        public static final int padding_large=0x7f060002;
+        public static final int padding_medium=0x7f060001;
+        public static final int padding_small=0x7f060000;
     }
     public static final class drawable {
         public static final int arrow1=0x7f020000;
@@ -86,20 +90,20 @@ public final class R {
         public static final int top_1=0x7f020024;
     }
     public static final class id {
-        public static final int btn_leftTop=0x7f090002;
-        public static final int btn_rightTop=0x7f090004;
-        public static final int item_image=0x7f090000;
-        public static final int item_textview=0x7f090001;
-        public static final int main_grid_view=0x7f090005;
-        public static final int main_tab_group=0x7f090007;
-        public static final int main_tab_host=0x7f090006;
-        public static final int main_tab_more=0x7f09000c;
-        public static final int main_tab_practise=0x7f090008;
-        public static final int main_tab_setting=0x7f09000b;
-        public static final int main_tab_sign=0x7f090009;
-        public static final int main_tab_technique=0x7f09000a;
-        public static final int menu_settings=0x7f09000d;
-        public static final int tv_head=0x7f090003;
+        public static final int btn_leftTop=0x7f0a0002;
+        public static final int btn_rightTop=0x7f0a0004;
+        public static final int item_image=0x7f0a0000;
+        public static final int item_textview=0x7f0a0001;
+        public static final int main_grid_view=0x7f0a0005;
+        public static final int main_tab_group=0x7f0a0007;
+        public static final int main_tab_host=0x7f0a0006;
+        public static final int main_tab_more=0x7f0a000c;
+        public static final int main_tab_practise=0x7f0a0008;
+        public static final int main_tab_setting=0x7f0a000b;
+        public static final int main_tab_sign=0x7f0a0009;
+        public static final int main_tab_technique=0x7f0a000a;
+        public static final int menu_settings=0x7f0a000d;
+        public static final int tv_head=0x7f0a0003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -112,24 +116,24 @@ public final class R {
         public static final int sign_grid_view=0x7f030007;
     }
     public static final class menu {
-        public static final int activity_main=0x7f080000;
-        public static final int activity_sign=0x7f080001;
+        public static final int activity_main=0x7f090000;
+        public static final int activity_sign=0x7f090001;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int bottom_tab_more=0x7f060008;
-        public static final int bottom_tab_practise=0x7f060004;
-        public static final int bottom_tab_setting=0x7f060007;
-        public static final int bottom_tab_sign=0x7f060005;
-        public static final int bottom_tab_technique=0x7f060006;
-        public static final int hello_world=0x7f060001;
-        public static final int menu_settings=0x7f060002;
-        public static final int title_activity_main=0x7f060003;
-        public static final int title_activity_sign=0x7f060009;
+        public static final int app_name=0x7f070000;
+        public static final int bottom_tab_more=0x7f070008;
+        public static final int bottom_tab_practise=0x7f070004;
+        public static final int bottom_tab_setting=0x7f070007;
+        public static final int bottom_tab_sign=0x7f070005;
+        public static final int bottom_tab_technique=0x7f070006;
+        public static final int hello_world=0x7f070001;
+        public static final int menu_settings=0x7f070002;
+        public static final int title_activity_main=0x7f070003;
+        public static final int title_activity_sign=0x7f070009;
     }
     public static final class style {
-        public static final int AppTheme=0x7f070000;
-        public static final int MMTabButton=0x7f070001;
-        public static final int MMTabButton1=0x7f070002;
+        public static final int AppTheme=0x7f080000;
+        public static final int MMTabButton=0x7f080001;
+        public static final int MMTabButton1=0x7f080002;
     }
 }
--- vLicense/src/libra/spicy/vlicense/MainActivity.java
+++ vLicense/src/libra/spicy/vlicense/MainActivity.java
@@ -1,119 +1,38 @@
 package libra.spicy.vlicense;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
+import libra.spicy.vlicense.utils.UIActivity;
+import libra.spicy.vlicense.utils.UIFactory;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.GridView;
-import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
-import android.widget.SimpleAdapter;
-import android.widget.TabHost;
+import android.widget.TextView;
 
-public class MainActivity extends Activity implements OnItemClickListener {
-	private int mCurrentSelectedTabId = R.id.main_tab_practise;
+public class MainActivity extends Activity  {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		GridView gridView = (GridView) findViewById(R.id.main_grid_view);
 
-		ArrayList<HashMap<String, Object>> gridviewData = new ArrayList<HashMap<String, Object>>();
+		TextView textview_header = (TextView) findViewById(R.id.tv_head);
 
-		for (int i = 1; i < 10; i++) {
-			HashMap<String, Object> grid = new HashMap<String, Object>();
-			grid.put("ItemImage", R.drawable.ic_launcher);
-			grid.put("ItemText", "Good" + i);
-			gridviewData.add(grid);
+		Intent intent = getIntent();
+		Bundle extras = intent.getExtras();
+
+		UIFactory uiFactory = UIFactory.getInstance(this);
+
+		// First time the application was lunched
+		if (extras == null) {
+			Log.d("my", "extra is null");
+			uiFactory.generateScreen(UIActivity.MainActivity, this);
+			textview_header.setText(UIActivity.MainActivity.toString());
+		} else {
+			UIActivity toActivity = (UIActivity) extras.get("ToActivity");
+			textview_header.setText(toActivity.toString());
+			uiFactory.generateScreen(toActivity, this);
 		}
-
-		SimpleAdapter gridViewAdapter = new SimpleAdapter(this, gridviewData,
-				R.layout.grid_cell, new String[] { "ItemImage", "ItemText" },
-				new int[] { R.id.item_image, R.id.item_textview });
-
-		gridView.setAdapter(gridViewAdapter);
-		gridView.setOnItemClickListener(this);
-
-		final TabHost tabHost = (TabHost) findViewById(R.id.main_tab_host);
-		tabHost.setup();
-		//
-		//
-		// TabSpec spec;
-		// spec = tabHost.newTabSpec(getString(R.string.bottom_tab_practise))
-		// .setIndicator(getString(R.string.bottom_tab_practise))
-		// .setContent(new Intent().setClass(this, SignActivity.class));
-		// Log.i("my", getString(R.string.bottom_tab_practise));
-		// tabHost.addTab(spec);
-		//
-		//
-		// spec = tabHost.newTabSpec(getString(R.string.bottom_tab_sign))
-		// .setIndicator(getString(R.string.bottom_tab_sign))
-		// .setContent(new Intent().setClass(this, SignActivity.class));
-		// tabHost.addTab(spec);
-		//
-		// spec = tabHost.newTabSpec(getString(R.string.bottom_tab_technique))
-		// .setIndicator(getString(R.string.bottom_tab_technique))
-		// .setContent(new Intent().setClass(this, SignActivity.class));
-		// tabHost.addTab(spec);
-		//
-		// spec = tabHost.newTabSpec(getString(R.string.bottom_tab_setting))
-		// .setIndicator(getString(R.string.bottom_tab_setting))
-		// .setContent(new Intent().setClass(this, SignActivity.class));
-		// tabHost.addTab(spec);
-		//
-		// spec = tabHost.newTabSpec(getString(R.string.bottom_tab_more))
-		// .setIndicator(getString(R.string.bottom_tab_more))
-		// .setContent(new Intent().setClass(this, SignActivity.class));
-		// tabHost.addTab(spec);
-
-		// tabHost.addTab(spec);
-		// tabHost.setCurrentTab(1);
-
-		RadioGroup radioGroup = (RadioGroup) findViewById(R.id.main_tab_group);
-		radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			public void onCheckedChanged(RadioGroup group, int checkedId) {
-				if (checkedId == mCurrentSelectedTabId) {
-					return;
-				}
-
-				Intent intent = new Intent();
-				switch (checkedId) {
-				case R.id.main_tab_practise:
-					tabHost.setCurrentTabByTag(getString(R.string.bottom_tab_practise));
-					intent.setClass(MainActivity.this, MainActivity.class);
-					break;
-				case R.id.main_tab_sign:
-					tabHost.setCurrentTabByTag(getString(R.string.bottom_tab_practise));
-					intent.setClass(MainActivity.this, MainActivity.class);
-					break;
-				case R.id.main_tab_technique:
-					tabHost.setCurrentTabByTag(getString(R.string.bottom_tab_practise));
-					intent.setClass(MainActivity.this, MainActivity.class);
-					break;
-				case R.id.main_tab_setting:
-					tabHost.setCurrentTabByTag(getString(R.string.bottom_tab_practise));
-					intent.setClass(MainActivity.this, MainActivity.class);
-					break;
-				case R.id.main_tab_more:
-					tabHost.setCurrentTabByTag(getString(R.string.bottom_tab_practise));
-					intent.setClass(MainActivity.this, MainActivity.class);
-					break;
-
-				default:
-					break;
-				}
-				startActivity(intent);
-			}
-		});
 
 	}
 
@@ -121,25 +40,6 @@ public class MainActivity extends Activity implements OnItemClickListener {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
-	}
-
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		Log.i("my", "arg2:" + arg2);
-
-		switch (arg2) {
-		case 0:
-			break;
-		case 1:
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		default:
-			break;
-		}
 	}
 
 }
